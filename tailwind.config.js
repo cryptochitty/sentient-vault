916a396a7f8be161c6148ad3d54bc9a6b2ee4069
CHITTY BABU M <109752565+cryptochitty@users.noreply.github.com>
2026-04-06 18:53:18 +0530

    Initial commit: Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        solana: {
+          purple: '#9945FF',
+          green: '#14F195',
+          dark: '#10141f'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
